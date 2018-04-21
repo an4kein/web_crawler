7be9b34cc236967e60787e29d74f064ff50f2068
Eduardo Barbosa (Anakein) <37910997+e-anakein@users.noreply.github.com>
2018-04-20 21:45:53 -0300

    Update to_crawl_html.py

--- to_crawl_html.py
+++ to_crawl_html.py
@@ -1,20 +1,29 @@
 import requests
 import re
 
-to_crawl = ['http://g1.globo.com']
-
+to_crawl = ['http://uol.com.br']
 crawled = set()
 
-req = requests.get(to_crawl[0])
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                        'AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/51.0.2704.103 Safari/537.36'}
 
-html = req.text
+while True:
+    url = to_crawl[0]
+    try:
+        req = requests.get(url, headers=header)
+    except:
+        to_crawl.remove(url)
+        crawled.add(url)
+        continue
 
+    html = req.text
+    links = re.findall(r'<a href="?\'?(https?:\/\/[^"\'>]*)', html)
+    print 'Crawling:', url
 
+    to_crawl.remove(url)
+    crawled.add(url)
 
-#padrao = re.search(r'href=[\'"][\w:/\.]+', html)
-padrao = re.findall(r'href=[\'"][\w:/\.]+', html)
-
-
-#print padrao.group()
-for link in padrao:
-    print link
+    for link in links:
+        if link not in crawled and link not in to_crawl:
+            to_crawl.append(link)
